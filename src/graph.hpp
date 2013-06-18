b71c74898d791131d68f1ab10ec3cd094d58b2ff
nikitoz <nikita.null@gmail.com>
2013-06-18 17:18:04 +0300

    graph was rewritten

--- src/graph.hpp
+++ src/graph.hpp
@@ -1,206 +1,145 @@
-#include <iostream>
 #include "bheap.hpp"
-#include <xutility>
+#include <vector>
+#include <map>
 
 namespace {
-	template <typename T>
-	struct ffgnode {
-		ffgnode(T node, T* next) {
-			data = node;
-			next = next;
-			valid = true;
-			cost = 0;
-			index = 0;
-		}
 
-		ffgnode(T node) {
-			data = node;
-			valid = true;
-			next = NULL;
-			cost = 0;
-			index = 0;
-		}
+	static const size_t INFINITY;
+	//=========================================================================
+	// Directed edge of a graph
+	//=========================================================================
+	template <typename TNode>
+	struct diedge {
+		TNode* _pFrom;
+		TNode* _pTo;
+		size_t _cost;
 
-		ffgnode() { 
-			valid = false;
-			cost = 0;
-			index = 0;
-			next = NULL;
-		}
+		diedge() 
+			: _pFrom(NULL), _pTo(NULL), _cost(0) {}
 
-		ffgnode(const ffgnode& prot, int cost) {
-			data = prot.data;
-			cost = cost;
-			next = NULL;
-			valid = true;
-			index = prot.index;
-		}
+		diedge(TNode* pFrom, TNode* pTo, size_t cost) 
+			: _pTo(pTo), _pFrom(pFrom), _cost(cost) {}
 
-		int			cost;
-		size_t		index;
-		ffgnode*	next;
+		size_t to() const
+		{ return _pTo->_index; }
 
-		T	 data;
-		bool valid;
+		size_t from() const
+		{ return _pFrom->_index; }
+
+		bool operator<(const diedge& d1) const
+		{ return _cost < d1._cost; }
+
+		bool operator>(const diedge& d1) const
+		{ return _cost > d1._cost; }
 	};
-
-	struct a_search_struct {
-		size_t index;
-		int cost;
-		size_t pred;
-
-		a_search_struct(size_t i, int c, size_t p) 
-		{ index = i; cost = c; pred = p; }
-		a_search_struct()
-		{ index = 0; cost = 0; pred = 0; }
-		void operator=(const a_search_struct& a1) 
-		{ index = a1.index; cost = a1.cost; pred = a1.pred; }
-		int getCost() const
-		{ return cost; }
-	};
-//=========================================================================
-//	ffgnode greater/less functors
-//=========================================================================
+	//=========================================================================
+	// Node of a graph
+	//=========================================================================
 	template <typename T>
-	class greater_cost {
-	public:
-		bool operator()(const T& node1, const T& node2)
-		{ return (node1.getCost() > node2.getCost()); }
-	};
+	struct gnode {
+		typedef gnode<T>                  node_t;
+		typedef diedge<node_t>            edge_t;
+		typedef std::map<size_t, edge_t > edges_t;
 
-	template <typename T>
-	class less_cost {
-	public:
-		bool operator()(const T& node1, const T& node2) const
-		{ return (node1.getCost() < node2.getCost()); }
+		gnode(T node, size_t index) 
+			: _data(node), _index(index) { }
+
+		gnode() 
+			: _index(0) { }
+
+		size_t  _index;
+		T	    _data;
+		edges_t _edges;// out from this node
 	};
 }
+//=========================================================================
+// Functional object for shortest path algo (uses A* inside)
+//=========================================================================
+template <typename TG>
+struct ff_shortest_path_astar {
 
-struct a_search_out {
-	size_t	 m_index;
-	a_search_out* m_next;
-	a_search_out(size_t index = -1, a_search_out* next = NULL)
-	{ m_index = index; m_next = next; }
+	struct edge_predicate {
+		bool operator()(const typename TG::edge_t& g1, const typename TG::edge_t& g2) const
+		{ return g1 < g2; }
+	};
+
+	size_t operator()( const TG& digraph, size_t src, size_t dst ) const {
+		size_t* cost = new size_t[digraph.count()];
+		size_t* esrc = new size_t[digraph.count()];
+
+		std::memset(cost, INFINITY, digraph.count()*sizeof(size_t));
+		std::memset(esrc, INFINITY, digraph.count()*sizeof(size_t));
+
+		cost[src] = 0;
+		ff_bheap<TG::edge_t, edge_predicate > bheap;
+		size_t current = src;
+
+		while (current != dst) {
+			for (TG::edges_t::const_iterator it = digraph.edges(current).begin(); it != digraph.edges(current).end(); ++it)
+					if (cost[it->second.to()] == INFINITY)
+						bheap.insert(it->second);
+
+			if (bheap.empty()) break;
+			TG::edge_t edge = bheap.pop();
+
+			cost[edge.to()] = edge._cost + cost[edge.from()];
+			esrc[edge.to()] = edge.from();
+			current         = edge.to();
+		}
+		size_t res = cost[dst];
+		delete[] cost;
+		delete[] esrc;
+		return res;
+	}
 };
 //=========================================================================
-// ffgraph
+// Class representing directed graph
 //=========================================================================
 template<typename TNode>
-class ff_directed_graph {
-
-typedef ffgnode<TNode>		node;
+class ff_digraph {
+	friend ff_shortest_path_astar< ff_digraph<TNode> >;
 
 public:
-	ff_directed_graph(size_t nodesCount) {
-		nodes = new node[nodesCount];
-		m_count = 0;
-		m_size   = nodesCount;
+	typedef typename gnode<TNode>		node_t;
+	typedef std::vector<node_t>			nodes_t;
+	typedef typename node_t::edge_t		edge_t;
+	typedef typename node_t::edges_t    edges_t;
+
+	ff_digraph() { }
+
+	ff_digraph(size_t count)
+	{ _nodes.reserve(count); }
+
+	size_t addNode(const TNode& data) {
+		_nodes.push_back(node_t(data, _nodes.size()));
+		return _nodes.size() - 1;
 	}
 
-	void addNode(const TNode& data) {
-		if (m_count == m_size) { // If we add to tyhe end, then resize array
-			node* buf = nodes;
-			nodes = new node[m_size*=2];
-			std::copy(buf, buf + m_count, nodes);
-			delete buf;
-		}
-			
-		nodes[m_count] = node(data);
-		nodes[m_count].index = m_count;
-		m_count++;
-	}
-
-	node getNode(int identifier) {
-		if (identifier >= m_count)
-			return node();
-		return nodes[identifier];
-	}
-
-	void addConnection(size_t fromId, size_t toId, int cost) {
-		if ((fromId >= m_count) || (toId >= m_count) || (toId == fromId))
+	void addConnection(size_t from, size_t to, int cost) {
+		if (from >= _nodes.size() || to >= _nodes.size() || to == from)
 			return;
 
-		node *newNode = new node(nodes[toId], cost);
-		node *next = nodes[fromId].next;
-		nodes[fromId].next = newNode; 
-		newNode->next = next;
-	}
-	
-	// return -1 if we don't have connection
-	int getCost(size_t fromId, size_t toId) {
-		if ((fromId >= m_count) || (toId >= m_count) || (toId == fromId))
-			return -1;
-
-		node* s = nodes+fromId;
-		const TNode& lookFor = nodes[toId].constValue();
-
-		while (s->next && s->next->data != lookFor) s = s->next;
-
-		if (s->next)
-			return s->next->cost;
-
-		return -1;
+		_nodes[from]._edges[to] = edge_t(&_nodes[from], &_nodes[to], cost);
 	}
 
-	// A* search
-	a_search_out* fillShortestPath(int from, int to) const {
-		if (! nodes[from].next ) 
-			return NULL;
+	size_t count() const
+	{ return _nodes.size();	}
 
-		ff_bheap<a_search_struct, less_cost<a_search_struct> > biHeap;
+	node_t node(size_t index) const
+	{ return _nodes[index]; }
 
-		size_t retValIndex = 0;
+	const nodes_t& nodes() const
+	{ return _nodes; }
 
-		node* current = nodes[from].next;
-		int currentIndex = from;
-		int additionalCost = 0;
+	bool hasConnection(size_t from, size_t to) const
+	{ return edges(from).find(to) != edges(from).end();	}
 
-		size_t *p = new size_t[m_count];
-		memset(p, 0, sizeof(int)*m_count);
+	edge_t edge(size_t from, size_t to) const
+	{ return _nodes[from]._edges[to]; }
 
-		// do while we don't get to our destination
-		do {
-			while (current != NULL) {
-				if (p[current->index]==0) // if we didnt' find path to this, then add
-					biHeap.insert(a_search_struct(current->index, current->cost + additionalCost, currentIndex));
-				current = current->next;
-			}
-
-			a_search_struct pret;
-			// retrieve best element, which is not found yet
-			do {
-				pret = biHeap.pop();
-			} while (p[pret.index]);
-
-			currentIndex = pret.index; // store index of our best element
-		//	retValIndex = pret.pred;   // save previous element	
-			p[pret.index] = pret.pred;	   // save cost to table
-		//	retVal[retValIndex++] = currentIndex; // save this element to path
-#if _DEBUG
-			std::cout << "currIndex :" << currentIndex << " addco: " << additionalCost << std::endl;
-#endif
-			additionalCost = pret.cost;           // get cost to current element
-			current = nodes[currentIndex].next; // get first neighbor of current element
-		} while (currentIndex != to);
-
-		//retVal[retValIndex] = -1;
-		
-		a_search_out *retVal = new a_search_out(to);
-		a_search_out *newStruct(NULL);
-
-		size_t id = to;
-		while (id != from) {
-			newStruct = new a_search_out(p[id], retVal);
-			retVal = newStruct;
-			id = p[id];
-		}
-
-		delete []p;
-		return retVal;
-	}
+	const edges_t& edges(size_t from) const
+	{ return _nodes[from]._edges; }
 
 private:
-	node* nodes;
-	size_t m_count;
-	size_t m_size;
+	nodes_t _nodes;
 };
--- src/graph_main.cpp
+++ src/graph_main.cpp
@@ -7,50 +7,32 @@
 #include "graph.hpp"
 
 bool testGraph() {
-	ff_directed_graph<int> graphy(10);
+	ff_digraph<int> graphy;//(10);
 
 	graphy.addNode(0);
 	graphy.addNode(1);
 	graphy.addNode(2);
 	graphy.addNode(3);
 	graphy.addNode(4);
-	graphy.addNode(5);
-	graphy.addNode(6);
-	graphy.addNode(7);
-	/*graphy.addConnection(0, 1, 1);
-	graphy.addConnection(1, 2, 3);
-	graphy.addConnection(0, 2, 1);
-	graphy.addConnection(0, 5, 25);
-	graphy.addConnection(2, 5, 23);
-	graphy.addConnection(2, 4, 2);
-	graphy.addConnection(4, 5, 10);
-	graphy.addConnection(4, 6, 45);
-	graphy.addConnection(5, 6, 20);*/
+// 	graphy.addNode(5);
+// 	graphy.addNode(6);
+// 	graphy.addNode(7);
+
 	graphy.addConnection(0, 1, 1);
 	graphy.addConnection(1, 2, 2);
-	graphy.addConnection(2, 3, 3);
+	graphy.addConnection(1, 3, 1);
+	graphy.addConnection(2, 4, 1);
 	graphy.addConnection(3, 4, 1);
-	graphy.addConnection(4, 5, 2);
-	graphy.addConnection(5, 0, 3);
-	graphy.addConnection(2, 5, 2);
-	graphy.addConnection(4, 1, 2);
-	graphy.addConnection(0, 6, 1);
-	graphy.addConnection(6, 5, 1);
-	graphy.addConnection(5, 3, 1);
+// 	graphy.addConnection(4, 5, 2);
+// 	graphy.addConnection(5, 0, 3);
+// 	graphy.addConnection(2, 5, 2);
+// 	graphy.addConnection(4, 1, 2);
+// 	graphy.addConnection(0, 6, 1);
+// 	graphy.addConnection(6, 5, 1);
+// 	graphy.addConnection(5, 3, 1);
 
-
-	a_search_out *arr;
-	arr = graphy.fillShortestPath(0, 4);
-	int i = 0;
-	while(arr != NULL) {
-		std::cout << arr->m_index << std::endl;
-		arr = arr->m_next;
-	}
-
-	/*if (graphy.getCost(1, 2) != 3) throw std::exception("hello");
-	if (graphy.getCost(0, 2) != 1) throw std::exception("hello");
-	if (graphy.getCost(0, 5) != 25) throw std::exception("hello");
-	if (graphy.getCost(2, 5) != 23) throw std::exception("hello");*/
+	size_t s = ff_shortest_path_Astar<ff_digraph<int> >()( graphy, 0, 4 );
+	std::cout << s << std::endl;
 
 	return true;
 }
